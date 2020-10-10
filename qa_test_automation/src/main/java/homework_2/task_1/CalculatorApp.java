88e7cff9089203370774e746e944a753180b955f
Daria Baleevskaia <baleevskaya@gmail.com>
2020-10-10 22:34:56 +0300

    Complete homework 2.

---
+++ qa_test_automation/src/main/java/homework_2/task_1/CalculatorApp.java
@@ -0,0 +1,11 @@
+package homework_2.task_1;
+
+import java.io.IOException;
+
+public class CalculatorApp {
+
+    public static void main(String[] args) throws IOException {
+        Calculator calculator = new Calculator();
+        calculator.start();
+    }
+}
---
+++ qa_test_automation/src/main/java/homework_2/task_1/MathOperations.java
@@ -0,0 +1,11 @@
+package homework_2.task_1;
+
+public enum MathOperations {
+    addition,
+    subtraction,
+    multiplication,
+    division,
+    power,
+    factorial,
+    fibonacci
+}
